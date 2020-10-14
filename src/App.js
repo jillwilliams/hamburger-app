1a1a98ed8c4a92c58a947d5bcc7dc58ffa79a371
jillwilliams <codeGeek.03.14@gmail.com>
2020-10-13 20:23:49 -0700

    draft

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Burger from './components/Burger'
+import "./components/BurgerStyle.css";
+
+const App = () => {
+    return (
+        <div className="burgerContent">
+            <Burger />
+        </div>
+    );
+}
+
+export default App;
